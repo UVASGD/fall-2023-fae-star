def726248a3abc546f2b524a9b71d5b1f9653c89
funderfuldev <0liammullins0@gmail.com>
2023-09-30 12:03:15 -0400

    fixed action cost ui
    didn't work with the scrollbar stuff, now it does

--- Assets/Scripts/Activators/DisplayActionCost.cs
+++ Assets/Scripts/Activators/DisplayActionCost.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DisplayActionCost : MonoBehaviour, IActivator
 {
     //Display the cost of an action by hiding the portion of the respective bar to show the change before and after
+    //TODO : Make this script reference the active character's stats to change the bar's width in accordance with current and max mana/health
     [Header("References")]
     [SerializeField] int actionListIndex; //What index in the action list it is. Definitely a way to automatically set this
     [SerializeField] int cost;
@@ -28,7 +29,7 @@ public class DisplayActionCost : MonoBehaviour, IActivator
         active = true;
 
         //Make the flashing "cost" bar keep the width of the original
-        backingCostFillImage.sizeDelta = new Vector2(fillImage.sizeDelta.x, fillImage.sizeDelta.y);
+        backingCostFillImage.sizeDelta = new Vector2(defaultFillImageWidth, fillImage.sizeDelta.y);
 
         //Make the actual bar go down to the width after using the proposed action
         //TODO : replace this lerp with the character's actual maximum mana/health
@@ -44,8 +45,17 @@ public class DisplayActionCost : MonoBehaviour, IActivator
         fillImage.sizeDelta = new Vector2(defaultFillImageWidth, fillImage.sizeDelta.y);
 
         //Hide the flashing bar representing the cost of the action
-        backingCostFillImage.sizeDelta = new Vector2(0, backingCostFillImage.sizeDelta.y);
+        backingCostFillImage.sizeDelta = new Vector2(defaultFillImageWidth, backingCostFillImage.sizeDelta.y);
 
         active = false;
+    }
+
+    public void Reset()
+    {
+        //Put the bar's width back to what it's supposed to be
+        fillImage.sizeDelta = new Vector2(defaultFillImageWidth, fillImage.sizeDelta.y);
+
+        //Hide the flashing bar representing the cost of the action
+        backingCostFillImage.sizeDelta = new Vector2(0, backingCostFillImage.sizeDelta.y);
     }
 }
