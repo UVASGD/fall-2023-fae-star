64320734187e8e4bac014e78f5bb94e89fec27e9
Clark Pagels <Cjpagels2@gmail.com>
2023-10-05 16:17:33 -0400

    basic data files
    boilerplate

---
+++ Assets/Scripts/WeaponInfo/WeaponInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+protected class WeaponInfo : MonoBehaviour
+{
+    string name;
+    int damage;
+    Sprite sprite; 
+}
