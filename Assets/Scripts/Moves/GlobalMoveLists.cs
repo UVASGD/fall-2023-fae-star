d759a4fb16f461f407d19d4107298f2a819934ec
ianharvey302 <iharv1@verizon.net>
2023-10-05 23:47:17 -0400

    Finished Enemy select transition (I forgot which one I was doing)

--- Assets/Scripts/Moves/GlobalMoveLists.cs
+++ Assets/Scripts/Moves/GlobalMoveLists.cs
@@ -6,35 +6,54 @@ using UnityEngine;
 public class GlobalMoveLists
 {
     // For now I am going to hard code this, but we might want to change it sometime in the future.
-    // The setup for this list is (int, Action, int) => (Mana Cost, Action on Select, Position in Selection List)
-    public static List<Dictionary<string, (int, Action, int)>> MoveList = new List<Dictionary<string, (int, Action, int)>>
+    // The setup for this list is (int, Action, int) => (Mana Cost, ActionType, Action on Select, Position in Selection List)
+    /*
+     * Action Type codex
+     * SE = Single Target Enemy Focused Action
+     * ME = AOE Target Enemy Focused Action
+     * SA = Single Target Ally Focused Action
+     * MA = AOE Target Ally Focused Action
+     * PB = Personal Buff
+     */
+
+    public enum ActionTypes
+    {
+        SE,
+        ME,
+        SA,
+        MA,
+        PB,
+        none,
+    }
+
+    public static List<Dictionary<string, (int, ActionTypes, Action, int)>> MoveList = new List<Dictionary<string, (int, ActionTypes, Action, int)>>
         { //Mage's list comes first
-	        new Dictionary<string, (int, Action, int)> 
+	        new Dictionary<string, (int, ActionTypes, Action, int)> 
             {
-                {"Club", (0, DoNothing, 11)},
-                {"Lesser Heal", (3, DoNothing, 21)},
-                {"Heal", (5, DoNothing, 31)},
-                {"Lesser Flame", (4, DoNothing, 41)},
-                {"Blazing", (6, DoNothing, 51)},
-                {"Back", (0, null, 61)},
+                {"Club", (0, ActionTypes.SE, DoNothing, 11)},
+                {"Lesser Heal", (3, ActionTypes.SA, DoNothing, 21)},
+                {"Heal", (5, ActionTypes.SA, DoNothing, 31)},
+                {"Lesser Flame", (4, ActionTypes.SE, DoNothing, 41)},
+                {"Blazing", (6, ActionTypes.PB, DoNothing, 51)},
+                {"Back", (0, ActionTypes.none, null, 61)}
             },
-            new Dictionary<string, (int, Action, int)>
+            new Dictionary<string, (int, ActionTypes, Action, int)>
             {
-                {"Knife", (0, DoNothing, 11)},
-                {"Backstab", (3, DoNothing, 21)},
-                {"Back", (0, null, 31)},
+                {"Knife", (0, ActionTypes.SE, DoNothing, 11)},
+                {"Backstab", (3, ActionTypes.SE, DoNothing, 21)},
+                {"Back", (0, ActionTypes.none, null, 31)}
             },
-            new Dictionary<string, (int, Action, int)>
+            new Dictionary<string, (int, ActionTypes, Action, int)>
             {
-                {"Poison Touch", (0, DoNothing, 11)},
-                {"Malware", (3, DoNothing, 21)},
-                {"Back", (0, null, 31)},
+                {"Poison Touch", (0, ActionTypes.SE, DoNothing, 11)},
+                {"Malware", (3, ActionTypes.SE, DoNothing, 21)},
+                {"Back", (0, ActionTypes.none, null, 31)}
             },
-            new Dictionary<string, (int, Action, int)>
+            new Dictionary<string, (int, ActionTypes, Action, int)>
             {
-                {"Slash", (0, DoNothing, 11)},
-                {"Raise Shield", (3, DoNothing, 21)},
-                {"Back", (0, null, 31)},
+                {"Slash", (0, ActionTypes.SE, DoNothing, 11)},
+                {"Raise Shield", (3, ActionTypes.PB, DoNothing, 21)},
+                {"Back", (0, ActionTypes.none, null, 31)}
             }
         };
 
--- Assets/Scripts/StateTransitions/TransitionManager.cs
+++ Assets/Scripts/StateTransitions/TransitionManager.cs
@@ -9,11 +9,20 @@ public class TransitionManager : MonoBehaviour
 {
     [SerializeField] GameObject[] characterObjects;
     private static List<GameObject> characters;
+
+    /*
+     * Transition Orders:
+     * 0. Character Select -> Action Select
+     * 1-4: Action Select -> Respective Character Actions
+     * 5: ????
+     */
     [SerializeField] GameObject[] transitionsObjects;
     private static List<Transition> transitions;
     // change later to use global storage
     [SerializeField] Slider manabarObject;
     private static Slider manabar;
+
+    private static int characterIndex;
 
     void Awake()
     {
@@ -32,15 +41,16 @@ public class TransitionManager : MonoBehaviour
     
     public static void processTransition(int selected)
     {
-        switch((GlobalStateTracker.battleState, GlobalStateTracker.currentAction))
+        switch (GlobalStateTracker.battleState)
         {
-            case (GlobalStateTracker.States.CharacterSelect, null):
+            case GlobalStateTracker.States.CharacterSelect:
                 GlobalStateTracker.battleState = GlobalStateTracker.States.ActionSelect;
+                characterIndex = selected;
                 transitions[0].Transition(selected);
                 break;
 
-            case (GlobalStateTracker.States.ActionSelect, null):
-                if (selected == 0)
+            case GlobalStateTracker.States.ActionSelect:
+                if (selected == 0) // Action button is selected
                 {
                     GlobalStateTracker.battleState = GlobalStateTracker.States.ActionMenuing;
                     if (GlobalStateTracker.currentEntity == characters[0]) 
@@ -52,38 +62,58 @@ public class TransitionManager : MonoBehaviour
                     else 
                         transitions[4].Transition(selected);
                 }
-                else if (selected == 1)
+                else if (selected == 1) // Item button is selected
                 {
                     //GlobalStateTracker.battleState = GlobalStateTracker.States.ItemMenuing;
                     // TODO Implement Item Menu, for now we will just do nothing
                 }
+                else if (selected == 2) // Guard button is selected
+                {
+
+                }
+                else // Back button is selected
+                {
+                    reverseTransition();
+                }
                 break;
 
-<<<<<<< HEAD
-            case (GlobalStateTracker.States.ActionMenuing, null):
-                if (selected == 0) //assumed to mean healing move is selected
+            case GlobalStateTracker.States.ActionMenuing:
+                KeyValuePair<string, (int, GlobalMoveLists.ActionTypes, Action, int)> selectedMove = GlobalMoveLists.MoveList[characterIndex].ElementAt(selected);
+                switch (selectedMove.Value.Item2)
                 {
-                    GlobalStateTracker.battleState = GlobalStateTracker.CharacterSelect;
-                    //rather shouldn't it be CharacterSelectTransition?
-                    transitions[2].Transition(selected); //what does this mean 
-                }
-                else if (selected == 1) {
-                    //assumed other actions from the action menu will be handled here
-                }
-=======
-            // Should eventually move this code to GlobalStateTracker.States.PostActionEntitySelect
-            case (GlobalStateTracker.States.ActionMenuing, null):
-                int characterIndex = 0;
-                for (characterIndex = 0; characterIndex < characters.Count; characterIndex++)
-                {
-                    if (GlobalStateTracker.currentEntity == characters[characterIndex])
-                        break; 
-                }
+                    case GlobalMoveLists.ActionTypes.SE: // Single target enemy action selected
+                        GlobalStateTracker.battleState = GlobalStateTracker.States.PostActionEntitySelect;
+                        GlobalStateTracker.currentAction = selectedMove.Key;
+                        transitions[5].Transition(selected);
 
-                KeyValuePair<string, (int, Action, int)> selectedMove = GlobalMoveLists.MoveList[characterIndex].ElementAt(selected);
-                manabar.value -= (float) selectedMove.Value.Item1 / 10;
+                        break;
+
+                    case GlobalMoveLists.ActionTypes.SA: // Single target ally action selected
+                        GlobalStateTracker.battleState = GlobalStateTracker.States.PostActionEntitySelect;
+                        GlobalStateTracker.currentAction = selectedMove.Key;
+
+                        break;
+
+                    case GlobalMoveLists.ActionTypes.PB:
+                    case GlobalMoveLists.ActionTypes.ME:
+                    case GlobalMoveLists.ActionTypes.MA:
+                        // Should just Act here as there is no need to select anyone
+                        reverseTransition();
+                        break;
+
+                    case GlobalMoveLists.ActionTypes.none:
+                        // Return back to Action Select Menu
+                        reverseTransition();
+                        break;
+
+                }
+                break;
+
+            // Should eventually move this code to GlobalStateTracker.States.PostActionEntitySelect
+            case GlobalStateTracker.States.PostActionEntitySelect:
+                KeyValuePair<string, (int, GlobalMoveLists.ActionTypes, Action, int)> move = GlobalMoveLists.MoveList[characterIndex].ElementAt(selected);
+                manabar.value -= (float) move.Value.Item1 / 10;
                 reverseTransition();
->>>>>>> develop
                 break;
 
             default:
@@ -120,6 +150,15 @@ public class TransitionManager : MonoBehaviour
             case GlobalStateTracker.States.ItemMenuing:
                 GlobalStateTracker.battleState = GlobalStateTracker.States.ActionSelect;
                 // TODO Implement Item Menu
+                break;
+
+            case GlobalStateTracker.States.PostActionEntitySelect:
+                if(GlobalStateTracker.currentAction != null)
+                {
+                    GlobalStateTracker.battleState = GlobalStateTracker.States.ActionMenuing;
+                    GlobalStateTracker.currentAction = null;
+                    transitions[5].ReverseTransition();
+                }
                 break;
         }
         Debug.Log(GlobalStateTracker.toString());
