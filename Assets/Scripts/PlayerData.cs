0599125bb9c50df255e67ae4cb3e896150f146ff
zoh-f <zohfais@gmail.com>
2023-10-12 20:39:12 -0400

    updated framework to work with strings

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,14 +5,22 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
+[Serializable]
+public class SaveData // holds the variables to be saved
+{
+    public string MoveList1, MoveList2, MoveList3, MoveList4; // this is what will be stored in the file
+}
+
 public class PlayerData : MonoBehaviour
 {
 
-    int intToSave; // the general variable that it should be able to access, later this will be replaced with actual code
+    string mageMoveList, knifeMoveList, hackerMoveList, knightMoveList; // the lists being used for the current playthrough
+
     private string FileLocation;
 
     public void Awake(){
         FileLocation = String.Concat(Application.persistentDataPath,"/SaveData.dat");
+        //mageMoveList = GlobalMoveLists.MoveList; gonna have to figure out how to do this lol
     }
 
     public void SaveGame(){
@@ -20,13 +28,13 @@ public class PlayerData : MonoBehaviour
         BinaryFormatter format = new BinaryFormatter();
         FileStream data = File.Create(FileLocation); // saves to C:\Users\[user]\AppData\LocalLow\[whatever the company is]\SaveData.dat
         SaveData s = new SaveData();
-        s.SavedInt = intToSave;
+        s.MoveList1 = mageMoveList;
         format.Serialize(data, s); // encrypts the data so it can't be read as text
         data.Close();
         Debug.Log("Data saved");
     }
 
-    public void LoadGame(){
+    public void LoadGame(){ // overrides the variables at the top of the script so they can be read by other scripts
 
         if(File.Exists(FileLocation))
         {
@@ -34,7 +42,7 @@ public class PlayerData : MonoBehaviour
             FileStream data = File.Open(FileLocation, FileMode.Open);
             SaveData s = (SaveData)format.Deserialize(data); // reads the data back and turns it into type SaveData
             data.Close();
-            intToSave = s.SavedInt; // overrides the variable used in the current playthrough
+            mageMoveList = s.MoveList1; // overrides the variable used in the current playthrough
             Debug.Log("Data loaded");
         }
         else
@@ -45,16 +53,11 @@ public class PlayerData : MonoBehaviour
 
         if(File.Exists(FileLocation)){
             File.Delete(FileLocation); // file is gone
-            intToSave = 0; // now to reset all the variables so they don't get put back if the player tries to save
+            mageMoveList = ""; // now to reset all the variables so they don't get put back if the player tries to save
+            // MAKE SURE TO RESET THIS TO THE DEFAULT VALUES, NOT BLANK
             Debug.Log("File deleted");
         }
         else
             Debug.Log("No file to delete");
     }
-
-    [Serializable]
-public class SaveData // holds the variables to be saved
-{
-    public int SavedInt; // placeholder to save the example int, should save individual move lists instead. this is what will be used in game
-}
 }
