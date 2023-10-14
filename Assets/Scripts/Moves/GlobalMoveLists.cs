9536e11eb560392b9da850eeb9dbb9ee172ad828
blairezh <113472440+blairezh@users.noreply.github.com>
2023-10-14 17:06:11 -0400

    moved actiontypes to move.cs

--- Assets/Scripts/Moves/GlobalMoveLists.cs
+++ Assets/Scripts/Moves/GlobalMoveLists.cs
@@ -5,26 +5,6 @@ using UnityEngine;
 
 public class GlobalMoveLists
 {
-    // For now I am going to hard code this, but we might want to change it sometime in the future.
-    // The setup for this list is (int, Action, int) => (Mana Cost, ActionType, Action on Select, Position in Selection List)
-    /*
-     * Action Type codex
-     * SE = Single Target Enemy Focused Action
-     * ME = AOE Target Enemy Focused Action
-     * SA = Single Target Ally Focused Action
-     * MA = AOE Target Ally Focused Action
-     * PB = Personal Buff
-     */
-
-    public enum ActionTypes
-    {
-        SE,
-        ME,
-        SA,
-        MA,
-        PB,
-        none,
-    }
     
     public static List<Dictionary<string, (int, ActionTypes, Action, int)>> MoveList = new List<Dictionary<string, (int, ActionTypes, Action, int)>>
         { //Mage's list comes first
