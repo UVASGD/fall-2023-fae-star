0ed834af058f06b89d8629f9e0318b975670dd2b
zoh-f <zohfais@gmail.com>
2023-11-03 23:05:01 -0400

    base saving system

--- Assets/Scripts/Moves/GlobalMoveLists.cs
+++ Assets/Scripts/Moves/GlobalMoveLists.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalMoveLists
+public class GlobalMoveLists : IDataPersistence // interface needed to enable saving/loading capacities
 {
     // For now I am going to hard code this, but we might want to change it sometime in the future.
     // The setup for this list is (int, Action, int) => (Mana Cost, Action on Select, Position in Selection List)
-    public static List<Dictionary<string, (int, Action, int)>> MoveList = new List<Dictionary<string, (int, Action, int)>>
+    public static List<SerializableDictionary<string, (int, Action, int)>> MoveList = new List<SerializableDictionary<string, (int, Action, int)>>
         { //Mage's list comes first
-	        new Dictionary<string, (int, Action, int)> 
+	        new SerializableDictionary<string, (int, Action, int)> 
             {
                 {"Club", (0, DoNothing, 11)},
                 {"Lesser Heal", (3, DoNothing, 21)},
@@ -18,19 +18,19 @@ public class GlobalMoveLists
                 {"Blazing", (6, DoNothing, 51)},
                 {"Back", (0, null, 61)},
             },
-            new Dictionary<string, (int, Action, int)>
+            new SerializableDictionary<string, (int, Action, int)>
             {
                 {"Knife", (0, DoNothing, 11)},
                 {"Backstab", (3, DoNothing, 21)},
                 {"Back", (0, null, 31)},
             },
-            new Dictionary<string, (int, Action, int)>
+            new SerializableDictionary<string, (int, Action, int)>
             {
                 {"Poison Touch", (0, DoNothing, 11)},
                 {"Malware", (3, DoNothing, 21)},
                 {"Back", (0, null, 31)},
             },
-            new Dictionary<string, (int, Action, int)>
+            new SerializableDictionary<string, (int, Action, int)>
             {
                 {"Slash", (0, DoNothing, 11)},
                 {"Raise Shield", (3, DoNothing, 21)},
@@ -41,4 +41,12 @@ public class GlobalMoveLists
 
     // Temp function that does nothing
     public static void DoNothing() { }
+
+    public void LoadData(SaveData data) {
+        MoveList = data.MoveList; 
+    }
+
+    public void SaveData(SaveData data) {
+        data.MoveList = MoveList; // copies this over to the script that saves all the data
+    }
 }
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+public class SaveData // holds the variables to be saved
+{
+    public List<SerializableDictionary<string, (int, Action, int)>> MoveList; // this is what will be stored in the file
+
+    public SaveData(){ // stores default values
+            this.MoveList = new List<SerializableDictionary<string, (int, Action, int)>>();
+    }
+}
+
